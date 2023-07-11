fdd4c953db0c3cbb58d751c48ca238b70e5b8bd7
Andrew Williams <andy@tensixtyone.com>
2023-07-11 20:58:04 +0100

    Support docker secrets

--- gocardless_membership_exporter/config.py
+++ gocardless_membership_exporter/config.py
@@ -1,8 +1,10 @@
 from typing import Literal
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(secrets_dir='/run/secrets')
+
     gocardless_environment: Literal["live", "sandbox"] = "live"
     gocardless_token: str
 
